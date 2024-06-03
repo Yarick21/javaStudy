d8ec9bd8422a3075d080a36dbea8238da789d05f
y.yachmenev <y.yachmenev@tinkoff.ru>
2024-06-03 15:30:21 +0300

    about declare variables

---
+++ src/main/DataTypes/BasedTypes.java
@@ -0,0 +1,18 @@
+package main.DataTypes;
+
+public class BasedTypes {
+
+    public static void main(String[] args) {
+        System.out.println(declareParam());
+    }
+
+    public static int declareParam() {
+
+        int firstParam = 1;
+        var secondParam = 2;
+        final int THIRD_PARAM = 3;
+
+        return firstParam + secondParam + THIRD_PARAM;
+    }
+
+}
