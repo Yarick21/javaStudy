361e63eeaf42e921e88b21a9355be3cb3c87df56
y.yachmenev <y.yachmenev@tinkoff.ru>
2024-06-24 19:47:26 +0300

    Решил пару задач

--- src/main/JavaBase/Array/ArrayTrain.java
+++ src/main/JavaBase/Array/ArrayTrain.java
@@ -1,13 +1,20 @@
 package main.JavaBase.Array;
 
-import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class ArrayTrain {
     public static void main(String[] args) {
-        double[] input = { 1, 5.2, 4, 0, -1 };
+        int[] digits = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15};
+        System.out.println(
+                countPositivesSumNegatives(digits)
+        );
+    }
 
-        System.out.println(sumNumber(input));
+    public static String test() {
+        return "test";
     }
 
     /*
@@ -45,5 +52,73 @@ public class ArrayTrain {
             }
         }
         return result;
+    }
+
+/*
+Рассмотрим массив/список овец, в котором некоторые овцы могут отсутствовать на своем месте.
+Нам нужна функция, которая подсчитывает количество овец, присутствующих в массиве (истина означает наличие).
+*/
+
+    static int sumSheeps(boolean[] sheeps) {
+        int result = 0;
+
+        for(int i = 0; i < sheeps.length; i++) {
+            if (sheeps[i]) {
+                result += 1;
+            }
+        }
+        System.out.println("Количество овец = " + result);
+
+        return result;
+    }
+
+    /**
+     * Учитывая массив целых чисел, ваше решение должно найти наименьшее целое число.
+     * Например: Учитывая [34, 15, 88, 2], ваше решение вернет 2 Учитывая [34, -345, -1, 100], ваше решение вернет -345.
+     * Для целей этого ката вы можете предположить, что предоставленный массив не будет пустым.
+     * */
+
+    static int minDigit(int[] digits) {
+        int min = digits[0];
+
+//        for(int digit: digits) {
+//            if(digit < min) {
+//                min = digit;
+//            }
+//        }
+        for(int i = 0; i < digits.length; i++) {
+            if(digits[i] < min) {
+                min = digits[i];
+            }
+        }
+        return min;
+    }
+
+    /**
+     * Дан массив целых чисел. Возвращает массив, где первый элемент — это количество положительных чисел, а второй элемент — сумма отрицательных чисел.
+     * 0 не является ни положительным, ни отрицательным. Если входные данные представляют собой пустой массив или имеют значение NULL, верните пустой массив.
+     * Пример Для ввода [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15] вы должны вернуть [10, -65].
+     */
+
+    public static int[] countPositivesSumNegatives(int[] input) {
+        int positiveDigit = 0;
+        int sumOfnegativeDigit = 0;
+
+        if((input.length == 0) || (Arrays.asList(input).contains(null))) {
+            return input;
+        } else {
+            for(int digit: input) {
+                if(digit == 0) {
+                    continue;
+                } else if (digit < 0) {
+                    sumOfnegativeDigit += digit;
+                } else if (digit > 0) {
+                    positiveDigit++;
+                }
+            }
+            int[] result = new int[] {positiveDigit, sumOfnegativeDigit};
+//            System.out.println(Arrays.toString(result));
+            return result;
+        }
     }
 }
