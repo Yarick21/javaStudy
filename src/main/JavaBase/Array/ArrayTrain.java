959bc6c5a0e05d3d59ef5e14c2c1f20a6ed01630
y.yachmenev <y.yachmenev@tinkoff.ru>
2024-07-15 17:50:50 +0300

    commit

--- src/main/JavaBase/Array/ArrayTrain.java
+++ src/main/JavaBase/Array/ArrayTrain.java
@@ -2,15 +2,14 @@ package main.JavaBase.Array;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
-import java.util.Scanner;
+import java.util.Set;
 
 public class ArrayTrain {
     public static void main(String[] args) {
-        int[] digits = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15};
-        System.out.println(
-                countPositivesSumNegatives(digits)
-        );
+        int arr[] = {1, 1, 2, 2, 3};
+        System.out.println(sumNoDuplicates(arr));
     }
 
     public static String test() {
@@ -121,4 +120,52 @@ public class ArrayTrain {
             return result;
         }
     }
+
+    /**
+     * Завершите метод, который принимает логическое значение и возвращает строку «Да» для значения true или строку «Нет» для значения false.
+     */
+    public static String boolToWord(boolean b) {
+        final String YES = "Yes";
+        final String NO = "No";
+
+        if (b) {
+            return YES;
+        } else {
+            return NO;
+        }
+    }
+
+    /**
+     * Это довольно просто. Ваша цель — создать функцию, которая удаляет первый и последний символы строки.
+     * Вам дан один параметр — исходная строка. Вам не нужно беспокоиться о строках, содержащих менее двух символов.
+     * */
+
+    public static String removeFirstLastElement(String str) {
+        ArrayList<String> result = new ArrayList<>(List.of(str.split("")));
+        result.remove(0);
+        result.remove(result.size() - 1);
+
+        return String.join("", result);
+    }
+
+    /**
+     * Напишите функцию, которая суммирует список, но игнорирует любые повторяющиеся элементы в списке.
+     * Например, для списка [3, 4, 3, 6] функция должна вернуть 10, а для списка [1, 10, 3, 10, 10] функция должна возвращать 4.
+     * */
+
+    static int sumNoDuplicates(int[] arr) {
+        int result = 0;
+        HashSet newList = new HashSet();
+
+        for(int arrValue: arr) {
+            newList.add(arrValue);
+        }
+        for(Object i: newList) {
+            result += (int)i;
+            System.out.println(i);
+        }
+        System.out.println(newList);
+        return result;
+    }
 }
+
